68e53f0d14e8969f5fe8064fa47bb89e4d6908a9
karan mehra <karan86.mehr@gmail.com>
2021-07-27 11:13:32 +0530

    added longest unique substring length in string java

---
+++ src/com/kmehra/leetcode/LeetLongestSubstringLength.java
@@ -0,0 +1,44 @@
+package com.kmehra.leetcode;
+
+import java.util.LinkedHashSet;
+
+public class LeetLongestSubstringLength {
+
+public static int findLongestSubstring(String str) {
+		
+		int maxSize = 0;
+		int start = 0;
+		int end = 0;
+		LinkedHashSet<Character> charSet = new LinkedHashSet<Character>();
+		
+		char[] charray = str.toCharArray();
+		
+		while(start < charray.length) {
+		
+			if(end < charray.length
+					&& !charSet.contains(charray[end])) {
+				
+				
+				charSet.add(charray[end]);
+				maxSize = Math.max(maxSize, charSet.size());
+				end++;
+			} else {
+				
+				charSet.remove(charray[start]);
+				start++;
+				
+			} 
+			
+		}
+		
+		return maxSize;
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		String str = "ritmschool";//"";//"bbbbbbbbb"; //"thecatinthehat"; //"thisisawesome";
+		int longestSize = findLongestSubstring(str);
+		System.out.println("The longest subarray size is : " + longestSize);
+	}
+}
